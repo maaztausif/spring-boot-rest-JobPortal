1fbc3c3a0d7df559edd29cfc563886e613832127
maaztausif <maaztausif48@gmail.com>
2025-09-04 17:22:21 +0200

    Project Created

---
+++ src/main/java/com/maaz/spring_boot_rest_JobPortal/SpringBootRestJobPortalApplication.java
@@ -0,0 +1,13 @@
+package com.maaz.spring_boot_rest_JobPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestJobPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestJobPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maaz/spring_boot_rest_JobPortal/SpringBootRestJobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maaz.spring_boot_rest_JobPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestJobPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
